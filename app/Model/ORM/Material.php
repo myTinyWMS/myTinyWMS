40a73e20c18be2db17d12108c6a649db63139b8e
Alessio Pani <alessio.pani@pls-europe.eu>
2017-10-24 10:59:48 +0200

    Added Material model

---
+++ app/Model/ORM/Material.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Model\ORM;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Model\ORM\Material
+ *
+ * @property integer $id
+ * @property string $bestnr
+ * @property integer $hersteller
+ * @property string $artikelbezeichnung
+ * @property integer $type
+ * @property integer $bestand
+ * @property integer $mindbestand
+ * @property integer $verbrauch
+ * @property integer $bestellzeitpunkt
+ * @property integer $maschinenzugehoerigkeit
+ * @property float $preis
+ * @property integer $bestellmenge
+ * @property string $bemerkung
+ * @property string $gefahrenklasse
+ * @property string $lieferzeit
+ * @property integer $status
+ * @property string $barcode
+ * @property integer $entnahmemenge
+ * @property string $barcode_file
+ * @property integer $mail_to_supplier
+ * @property integer $bestell_status
+ * @property Carbon $date
+ * @property integer $sort_id
+ * @property boolean $inventur
+ */
+class Material extends Model {
+    protected $table = "material";
+    protected $connection = 'onp';
+    public $incrementing = true;
+    public $timestamps = false;
+    protected $guarded = [];
+}
