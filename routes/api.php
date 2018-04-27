54d1faa14ef3f332f27ce113d7f539ffdd1d82f4
alex <alexander.reichardt@pls-europe.eu>
2018-04-27 08:46:04 +0200

    bugfix

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth.basic.stateless'])->namespace('Api')->group(function () {
-    Route::get('/user', function (Request $request) {
+    /*Route::get('/user', function (Request $request) {
         return $request->user();
-    });
+    });*/
 
     Route::resource('material', 'ApiMaterialController', ['only' => [
         'show', 'update'
