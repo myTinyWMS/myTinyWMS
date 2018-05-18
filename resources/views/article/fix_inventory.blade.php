c423a2eda9cdb308151d33168b8ed706bcea00c9
alex <alexander.reichardt@pls-europe.eu>
2018-05-18 11:14:04 +0200

    show notes in mass article update

--- resources/views/article/fix_inventory.blade.php
+++ resources/views/article/fix_inventory.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <form method="post" action="{{ route('article.fix_inventory_save') }}">
         <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
             @foreach($articles as $category => $items)
             <div class="ibox">
                 <div class="ibox-title">
@@ -16,8 +16,9 @@
                         <thead>
                             <tr>
                                 <th>Artikel</th>
-                                <th width="30%">Lieferant</th>
-                                <th width="30%">Einheit</th>
+                                <th width="20%">Lieferant</th>
+                                <th width="30%">Notizen</th>
+                                <th width="10%">Einheit</th>
                                 <th width="10%" class="text-center">Inventur</th>
                             </tr>
                         </thead>
@@ -26,6 +27,7 @@
                             <tr>
                                 <td>{{ $article->name }}</td>
                                 <td>{{ $article->supplier_name }}</td>
+                                <td>{{ $article->notes }}</td>
                                 <td>
                                     {{ Form::bsSelect('unit_id['.$article->id.']', $article->unit_id, $units,  '', ['placeholder' => '']) }}
                                 </td>
