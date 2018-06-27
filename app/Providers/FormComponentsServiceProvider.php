600e6f94d23d53da9788de30a44fef425add1da0
alex <alexander.reichardt@pls-europe.eu>
2018-06-27 15:05:54 +0200

    added worker container

--- app/Providers/FormComponentsServiceProvider.php
+++ app/Providers/FormComponentsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Mss\Providers;
 
 use Collective\Html\FormFacade as Form;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class FormComponentsServiceProvider extends ServiceProvider
@@ -11,16 +12,18 @@ class FormComponentsServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot() {
-        Form::component('bsText', 'components.form.text', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
-        Form::component('bsPassword', 'components.form.password', ['name', 'attributes' => [], 'label' => null, 'helpText' => null]);
-        Form::component('aceHTML', 'components.form.ace_html', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
-        Form::component('summernote', 'components.form.summernote', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
-        Form::component('bsRoleSelect', 'components.form.select_role', ['name', 'values' => collect([]), 'attributes' => [], 'label' => null]);
-        Form::component('bsCheckbox', 'components.form.checkbox', ['name', 'value' => null, 'label' => null, 'checked' => false, 'attributes' => [], 'parentClasses' => '']);
-        Form::component('bsFile', 'components.form.file', ['name', 'label' => '', 'helpBlock' => '', 'attributes' => []]);
-        Form::component('bsSelect', 'components.form.select', ['name', 'value' => null, 'values' => collect([]), 'label' => '', 'attributes' => []]);
-        Form::component('bsTextarea', 'components.form.textarea', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
-        Form::component('dropzone', 'components.form.dropzone', ['name', 'label' => null, 'url' => null]);
+        if (!App::runningInConsole()) {
+            Form::component('bsText', 'components.form.text', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
+            Form::component('bsPassword', 'components.form.password', ['name', 'attributes' => [], 'label' => null, 'helpText' => null]);
+            Form::component('aceHTML', 'components.form.ace_html', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
+            Form::component('summernote', 'components.form.summernote', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
+            Form::component('bsRoleSelect', 'components.form.select_role', ['name', 'values' => collect([]), 'attributes' => [], 'label' => null]);
+            Form::component('bsCheckbox', 'components.form.checkbox', ['name', 'value' => null, 'label' => null, 'checked' => false, 'attributes' => [], 'parentClasses' => '']);
+            Form::component('bsFile', 'components.form.file', ['name', 'label' => '', 'helpBlock' => '', 'attributes' => []]);
+            Form::component('bsSelect', 'components.form.select', ['name', 'value' => null, 'values' => collect([]), 'label' => '', 'attributes' => []]);
+            Form::component('bsTextarea', 'components.form.textarea', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
+            Form::component('dropzone', 'components.form.dropzone', ['name', 'label' => null, 'url' => null]);
+        }
     }
 
     /**
---
+++ docker/worker/laravel-worker.conf.tpl
@@ -0,0 +1,11 @@
+[supervisord]
+nodaemon=true
+
+[program:laravel-worker]
+process_name=%(program_name)s_%(process_num)02d
+command=php /data/www/artisan queue:work %%QUEUE_CONNECTION%% --queue=%%QUEUE_NAME%%
+autostart=true
+autorestart=true
+numprocs=1
+startretries=10
+stdout_events_enabled=1
