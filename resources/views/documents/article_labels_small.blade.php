b5c3eeae61912cdf26936248c04a544603b800e3
alex <alexander.reichardt@pls-europe.eu>
2018-07-04 15:07:27 +0200

    fixed small label

--- resources/views/documents/article_labels_small.blade.php
+++ resources/views/documents/article_labels_small.blade.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-    @import 'https://fonts.googleapis.com/css?family=Roboto+Mono';
+    @import 'https://fonts.googleapis.com/css?family=Inconsolata';
 
     div.page {
         page-break-inside: avoid;
@@ -17,6 +17,6 @@
     <div style="width: 170px;  float: right; text-align: center;">
         <span style="font-size: 43px;">{{ $article->article_number ?? '#'.$article->id }}</span>
     </div>
-    <div style="clear: both; font-size: 16px; line-height: 18px; padding-top: 7px; font-family: 'Roboto Mono';word-break: break-all;">{{ substr(preg_replace('/[\r\n\t]/', '', $article->name), 0, 52) }}</div>
+    <div style="clear: both; font-size: 16px; line-height: 18px; padding-top: 5px; font-family: Inconsolata;word-break: break-all;">{{ substr(preg_replace('/[\r\n\t]/', '', $article->name), 0, 52) }}</div>
 </div>
 @endforeach
