08bbfc8a45d8f08230cac4d26dd7a27a152935ca
Alexander Reichardt <eagledata@gmail.com>
2019-01-21 16:55:21 +0100

    bugfix

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -347,7 +347,7 @@ class Article extends AuditableModel
         $previousArticleSupplierAudits = null;
         $articleSupplierAudits = $this->supplierArticles->map(function ($item) use (&$previousArticleSupplierAudits) {
             /* @var $item ArticleSupplier */
-            $audits = $item->getAudits();
+            $audits = $item->getAudits()->sort('timestamp');
 
             if ($previousArticleSupplierAudits && $audits->count() && collect($audits->first())->get('modified')->has('supplier_id')) {
                 $audits->transform(function ($audit) use ($previousArticleSupplierAudits) {
