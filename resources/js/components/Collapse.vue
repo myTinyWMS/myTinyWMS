d9a098ab04f461da8c0d3c0b265ccdfc3da3b6ab
Alexander Reichardt <eagledata@gmail.com>
2019-05-27 21:43:47 +0200

    fixed collaps

--- resources/js/components/Collapse.vue
+++ resources/js/components/Collapse.vue
@@ -2,8 +2,8 @@
     <div class="card">
         <div class="card-header flex text-grey-dark">
             <h4 class="flex-1">{{ title }}</h4>
-            <z icon="cheveron-down" class="h-6 w-6 fill-current text-grey-dark cursor-pointer" v-show="open" @click="open = false"></z>
-            <z icon="cheveron-right" class="h-6 w-6 fill-current text-grey-dark cursor-pointer" v-show="!open" @click="open = true"></z>
+            <a href="javascript:void(0)" v-show="open" @click="open = false"><z icon="cheveron-down" class="h-6 w-6 fill-current text-grey-dark cursor-pointer"></z></a>
+            <a href="javascript:void(0)" v-show="!open" @click="open = true"><z icon="cheveron-right" class="h-6 w-6 fill-current text-grey-dark cursor-pointer"></z></a>
         </div>
         <div class="card-content" v-show="open">
             <slot></slot>
