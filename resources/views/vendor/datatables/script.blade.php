6d8f178f772ba38bee5e06b461fb25aded391e6d
Alexander Reichardt <eagledata@gmail.com>
2019-06-10 21:32:49 +0200

    fixed JS

--- resources/views/vendor/datatables/script.blade.php
+++ resources/views/vendor/datatables/script.blade.php
@@ -2,5 +2,5 @@
     window.LaravelDataTables=window.LaravelDataTables||{};
     window.DataTableConfig = %2$s;
     window.DataTableId = "%1$s";
-    window.LaravelDataTables[window.DataTableId]=$(window.DataTableId).DataTable(window.DataTableConfig);
+    window.LaravelDataTables[window.DataTableId]=$('#'+window.DataTableId).DataTable(window.DataTableConfig);
 })(window,jQuery);
