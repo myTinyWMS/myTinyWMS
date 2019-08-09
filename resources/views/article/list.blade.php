68fd6926265687218b421bcbfa4f0950bf08da52
Alexander Reichardt <eagledata@gmail.com>
2019-08-09 11:47:25 +0200

    removed class

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -5,7 +5,7 @@
 @section('title_extra')
     <div>
         <div class="dropdown-list group">
-            <div class="dropdown-list-header">
+            <div class="flex items-center cursor-pointer text-sm text-blue-500 rounded-t-lg py-1 px-2">
                 weitere Aktionen
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
             </div>
@@ -28,7 +28,7 @@
 
 <div class="footer_actions hidden">
     <div class="dropdown-list group">
-        <div class="dropdown-list-header">
+        <div class="flex items-center cursor-pointer text-sm text-blue-500 rounded-t-lg py-1 px-2">
             Aktion
             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
