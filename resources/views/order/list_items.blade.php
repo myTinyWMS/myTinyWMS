3810f54769e3f6c445ce1a52c11a3e7391e5dd79
Alexander Reichardt <eagledata@gmail.com>
2019-08-09 11:46:36 +0200

    changed text color in orders table

--- resources/views/order/list_items.blade.php
+++ resources/views/order/list_items.blade.php
@@ -1,5 +1,5 @@
 <div class="dropdown-list group" style="width: 800px">
-    <div class="dropdown-list-header font-bold">
+    <div class="flex items-center cursor-pointer text-sm rounded-t-lg py-1 px-2 font-bold text-gray-600">
         {{ $order->items->count() }} Artikel
         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
     </div>
