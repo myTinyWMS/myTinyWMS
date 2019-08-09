7e4dfd076fcfaf6e3499cfe354c4bdb2c7c0b238
Alexander Reichardt <eagledata@gmail.com>
2019-08-09 08:32:49 +0200

    fix for text only body

--- app/Http/Controllers/Order/MessageController.php
+++ app/Http/Controllers/Order/MessageController.php
@@ -33,7 +33,8 @@ class MessageController extends Controller {
             $orgMessage = OrderMessage::find(request('answer'));
             $preSetSubject = 'RE: '.$orgMessage->subject;
             $preSetReceiver = $orgMessage->sender->contains('System') ? '' : $orgMessage->sender->implode(',');
-            $preSetBody = '<br/><br/>Am '.$orgMessage->received->formatLocalized('%A, %d.%B %Y, %H:%M Uhr').' schrieb '.($orgMessage->sender->contains('System') ? env('MAIL_FROM_ADDRESS') : $orgMessage->sender->first()).':<br/><blockquote style="padding: 10px 20px;margin: 5px 0 20px;border-left: 5px solid #eee;">'.$orgMessage->htmlBody.'</blockquote>';
+            $body = (empty($orgMessage->htmlBody)) ? $orgMessage->textBody : $orgMessage->htmlBody;
+            $preSetBody = '<br/><br/>Am '.$orgMessage->received->formatLocalized('%A, %d.%B %Y, %H:%M Uhr').' schrieb '.($orgMessage->sender->contains('System') ? env('MAIL_FROM_ADDRESS') : $orgMessage->sender->first()).':<br/><blockquote style="padding: 10px 20px;margin: 5px 0 20px;border-left: 5px solid #eee;">'.$body.'</blockquote>';
         }
 
         $sendOrder = false;
