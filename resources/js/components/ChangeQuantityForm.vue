bca1fed9e0fbbebb91b27ac2e5be26bb26364235
Alexander Reichardt <eagledata@gmail.com>
2019-10-09 10:00:36 +0200

    fixed js issue

--- resources/js/components/ChangeQuantityForm.vue
+++ resources/js/components/ChangeQuantityForm.vue
@@ -53,7 +53,7 @@
             <textarea class="form-textarea" rows="3" id="changelogNote" name="changelogNote"></textarea>
         </div>
 
-        <div v-if="(article.delivery_notes.length > 0 && changelogType.value == 1)">
+        <div v-if="(typeof article.delivery_notes == 'string' && article.delivery_notes.length > 0 && changelogType.value == 1)">
             <span class="font-semibold text-red-500 text-xs">{{ article.delivery_notes }}</span>
         </div>
 
