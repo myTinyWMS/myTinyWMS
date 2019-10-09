9b304ded25a85f20d207833944bd26704ab2e646
Alexander Reichardt <eagledata@gmail.com>
2019-10-09 12:08:55 +0200

    updated deps, use full article name as title in search results

--- app/Http/Controllers/GlobalSearchController.php
+++ app/Http/Controllers/GlobalSearchController.php
@@ -17,18 +17,18 @@ class GlobalSearchController extends Controller
         if (preg_match('/^[0-9]{7,8}$/', $phrase)) {
             $order = Order::where('internal_order_number', $phrase)->first();
             if ($order) {
-                $this->addResult('Bestellung', $phrase, route('order.show', $order), $order->internal_order_number);
+                $this->addResult('Bestellung', $phrase, route('order.show', $order));
             }
         } elseif (preg_match('/^[0-9]{5}$/', $phrase)) {
             $article = Article::where('article_number', $phrase)->first();
             if ($article) {
-                $this->addResult('Artikel', $article->name.' ('.$phrase.')', route('article.show', $article), $article->article_number);
+                $this->addResult('Artikel', $article->name.' ('.$phrase.')', route('article.show', $article));
             }
         } else {
             $articles = Article::where('name', 'like', '%'.$phrase.'%')->get();
             if ($articles) {
                 $articles->each(function ($article) {
-                    $this->addResult('Artikel', $article->name, route('article.show', $article), $article->article_number);
+                    $this->addResult('Artikel', $article->name, route('article.show', $article));
                 });
             }
         }
@@ -36,13 +36,13 @@ class GlobalSearchController extends Controller
         return response()->json(collect($this->results)->values());
     }
 
-    protected function addResult($group, $name, $link, $title) {
+    protected function addResult($group, $name, $link) {
         if (!array_key_exists($group, $this->results)) {
             $this->results[$group] = [
                 'name' => $group,
                 'items' => []
             ];
         }
-        $this->results[$group]['items'][] = ['name' => Str::limit($name, 50), 'link' => $link, 'title' => $title];
+        $this->results[$group]['items'][] = ['name' => Str::limit($name, 50), 'link' => $link, 'title' => $name];
     }
 }
