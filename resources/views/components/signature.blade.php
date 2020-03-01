50d1fe9676cc28bda60220bc1b5587065484c384
Alexander Reichardt <eagledata@gmail.com>
2020-03-01 22:13:57 +0100

    cleanup

--- resources/views/components/signature.blade.php
+++ resources/views/components/signature.blade.php
@@ -2,19 +2,3 @@ Mit freundlichen Grüßen<br/>
 <br/>
 {{ \Illuminate\Support\Facades\Auth::user()->name }}<br>
 Abt. Einkauf<br>
-<br>
-Tel: +49(0)1234 56789012<br>
-Fax: +49(0)1234 56789012<br>
-<br>
-<br>
-<br>
-<br>
-Test Firma,<br>
-Teststraße 1, 01234 Test<br>
-<br>
-Rechnungsadresse:<br>
-<br>
-E-Mail: mail@example.com<br>
-Eingetragen beim Amtsgericht Dresden, <br>
-Geschäftsführer/Managing Director: <br>
-USt.-Id.Nr: <br>
