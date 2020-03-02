1ac4fe83037994d031f9df9c2b52b2d99c5c658f
Alexander Reichardt <eagledata@gmail.com>
2020-03-02 22:21:07 +0100

    added https force fix for datatables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -67,6 +67,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
+        if (env('APP_ENV') === 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
