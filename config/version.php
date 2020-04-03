51782114b249dc5edd5eabb5e168392a07b556f2
Alexander Reichardt <eagledata@gmail.com>
2020-04-03 16:25:06 +0200

    bumped version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.2.1',
-  'full_app_version' => 'v1.2.1 - build 913-gb800d5b',
-  'build_version' => '913',
+  'app_version' => 'v1.2.2',
+  'full_app_version' => 'v1.2.2 - build 917-g0ec270f',
+  'build_version' => '917',
   'prerelease_version' => '',
-  'hash_version' => 'gb800d5b',
-  'full_hash' => 'v1.2.0-2-gb800d5b',
+  'hash_version' => 'g0ec270f',
+  'full_hash' => 'v1.2.1-3-g0ec270f',
   'branch' => 'master',
 );
