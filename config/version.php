e2614af2374fd7e8607c9c0d2bca2500fc919126
Alexander Reichardt <eagledata@gmail.com>
2020-04-07 23:05:37 +0200

    removed xdebug

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.2.3',
-  'full_app_version' => 'v1.2.3 - build 919-g17d2697',
-  'build_version' => '919',
+  'app_version' => 'v1.2.4',
+  'full_app_version' => 'v1.2.4 - build 925-g92e97e3',
+  'build_version' => '925',
   'prerelease_version' => '',
-  'hash_version' => 'g17d2697',
-  'full_hash' => 'v1.2.2-1-g17d2697',
+  'hash_version' => 'g92e97e3',
+  'full_hash' => 'v1.2.3-5-g92e97e3',
   'branch' => 'master',
 );
