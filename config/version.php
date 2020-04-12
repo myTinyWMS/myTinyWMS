fa155c47f165d17bf7a69df587995f8111012d11
Alexander Reichardt <eagledata@gmail.com>
2020-04-12 22:58:09 +0200

    new version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.2.4',
-  'full_app_version' => 'v1.2.4 - build 925-g92e97e3',
-  'build_version' => '925',
+  'app_version' => 'v1.2.5',
+  'full_app_version' => 'v1.2.5 - build 1009-gc34c52f',
+  'build_version' => '1009',
   'prerelease_version' => '',
-  'hash_version' => 'g92e97e3',
-  'full_hash' => 'v1.2.3-5-g92e97e3',
+  'hash_version' => 'gc34c52f',
+  'full_hash' => 'v1.2.4-83-gc34c52f',
   'branch' => 'master',
 );
