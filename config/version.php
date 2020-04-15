2efae9b9c902bf50511c3a61e8ee81606b4ab1e1
Alexander Reichardt <alexander.reichardt@icloud.com>
2020-04-15 23:08:09 +0200

    bumped version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.4.0',
-  'full_app_version' => 'v1.4.0 - build 1029-g01365ac',
-  'build_version' => '1029',
+  'app_version' => 'v1.4.1',
+  'full_app_version' => 'v1.4.1 - build 1032-g09759b7',
+  'build_version' => '1032',
   'prerelease_version' => '',
-  'hash_version' => 'g01365ac',
-  'full_hash' => 'v1.4.0-1-g01365ac',
+  'hash_version' => 'g09759b7',
+  'full_hash' => 'v1.4.0-2-g09759b7',
   'branch' => 'master',
 );
