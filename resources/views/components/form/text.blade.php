56850f9778beddfbfe0894704489c4a481627a3d
Alexander Reichardt <eagledata@gmail.com>
2020-05-11 15:16:22 +0200

    added placeholder styling

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -1,8 +1,8 @@
 @if(!empty($label))
 <div class="form-group">
     {!! Form::label($name, $label, ['class' => 'form-label']) !!}
-    {!! Form::text($name, $value, array_merge(['class' => 'form-input'], $attributes)) !!}
+    {!! Form::text($name, $value, array_merge(['class' => 'form-input placeholder-gray-600'], $attributes)) !!}
 </div>
 @else
-    {!! Form::text($name, $value, array_merge(['class' => 'form-input'], $attributes)) !!}
+    {!! Form::text($name, $value, array_merge(['class' => 'form-input placeholder-gray-600'], $attributes)) !!}
 @endif
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,8 +1,8 @@
 @if(!empty($label))
 <div class="form-group">
     {!! Form::label($name, $label, ['class' => 'form-label']) !!}
-    {!! Form::textarea($name, $value, array_merge(['class' => 'form-textarea'], $attributes)) !!}
+    {!! Form::textarea($name, $value, array_merge(['class' => 'form-textarea placeholder-gray-600'], $attributes)) !!}
 </div>
 @else
-    {!! Form::textarea($name, $value, array_merge(['class' => 'form-textarea'], $attributes)) !!}
+    {!! Form::textarea($name, $value, array_merge(['class' => 'form-textarea placeholder-gray-600'], $attributes)) !!}
 @endif
