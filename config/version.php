13dc5318eb6932aa5396ec8b65fd6bc7ce725c89
Alex <info@alexander-adam.net>
2021-01-29 23:20:15 +0100

    bump version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.5.0',
-  'full_app_version' => 'v1.5.0 - build 1081-gd3d8138',
-  'build_version' => '1081',
+  'app_version' => 'v1.5.1',
+  'full_app_version' => 'v1.5.1 - build 1089-ga329c97',
+  'build_version' => '1089',
   'prerelease_version' => '',
-  'hash_version' => 'gd3d8138',
-  'full_hash' => 'v1.5.0-33-gd3d8138',
+  'hash_version' => 'ga329c97',
+  'full_hash' => 'v1.5.1-7-ga329c97',
   'branch' => 'master',
 );
