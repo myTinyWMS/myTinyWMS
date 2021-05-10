269f4ec1b2bddeeb1a6e8dd793292612859929b5
Alex <info@alexander-adam.net>
2021-05-10 22:26:53 +0200

    updated dependencies

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.5.3',
-  'full_app_version' => 'v1.5.3 - build 1096-',
-  'build_version' => '1096',
+  'app_version' => 'v1.5.7',
+  'full_app_version' => 'v1.5.7 - build 1099-',
+  'build_version' => '1099',
   'prerelease_version' => '',
   'hash_version' => '',
-  'full_hash' => 'v1.5.2',
+  'full_hash' => 'v1.5.7',
   'branch' => 'master',
 );
