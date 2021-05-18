8cd42f425c5935443f4348e88af2964811e76e49
Alex <info@alexander-adam.net>
2021-05-18 21:15:28 +0200

    updated version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v1.5.7',
-  'full_app_version' => 'v1.5.7 - build 1099-',
-  'build_version' => '1099',
+  'app_version' => 'v1.5.6',
+  'full_app_version' => 'v1.5.6 - build 1100-g05c0b77',
+  'build_version' => '1100',
   'prerelease_version' => '',
-  'hash_version' => '',
-  'full_hash' => 'v1.5.7',
+  'hash_version' => 'g05c0b77',
+  'full_hash' => 'v1.5.5-1-g05c0b77',
   'branch' => 'master',
 );
